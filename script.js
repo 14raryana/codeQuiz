ac9922a156189f9ebba8fa30bafa672c6c1981ce
Raman Aryana <14raryana@gmail.com>
2020-09-22 20:20:21 -0400

    almost finished. only things that are left are cosmetic, maybe some validation too

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
-var timerSeconds = 5;
+var timerSeconds = 20;
 $(".card-header").hide();
 var timer;
 var questionNumber = 0;
+var correctAnswersCount = 0;
+var wrongAnswersCount = 0;
+var calculatedScore;
 $("#startQuizAlign").css("text-align", "center");
 
 var questions = [
@@ -16,36 +19,45 @@ var questions = [
         choices: ["Both the <head> section and the <body> section are correct", "the <body> section", "The <head> section"],
         answer: "the <body> section"
     },
+
     {
         question: "The external JavaScript file must contain the <script> tag.",
         choices: ["True", "False"],
         answer: "False"
     }
-]
+];
+
 
 $("#startQuiz").on("click", function() {
     $(".card-header").show();
     $("#startQuiz").hide();
     $("#timer").text(timerSeconds);
-    timer = setInterval(timerSecondsFunction,1000);
+    timer = setInterval(countDownFunction,1000);
     question();
 });
 
-$("#nextQuestion").on("click", function(){
+// $("#nextQuestion").on("click", function(){
+//     questionNumber++;
+//     $("#answerList").empty();
+//     question();
+// });
+
+
+function countDownFunction() {
+    timerSeconds--;
+    $("#timer").text(timerSeconds);
+    if(timerSeconds <= 0) {
+        $("#timer").text("YOU FAIL, YOU NO DOCTOR!");
+        clearInterval(timer);
+        $(".card").empty();
+        storeInitials();
+    }
+}
+
+function nextQuestion() {
     questionNumber++;
     $("#answerList").empty();
     question();
-});
-
-
-
-function timerSecondsFunction() {
-    timerSeconds--;
-    $("#timer").text(timerSeconds);
-    if(timerSeconds == 0) {
-        $("#timer").text("YOU FAIL, YOU NO DOCTOR!");
-        clearInterval(timer);
-    }
 }
 
 function question() {
@@ -54,17 +66,85 @@ function question() {
     
     // var answerList = $("#answers").append("<ol></ol>");
     // answerList.attr("id", "answerList");
-    $("#answerList").attr("type","A")
+    $("#answerList").attr("type","A");
+
+    var currentQuestion = questions[questionNumber];
 
     for (var i = 0; i < questions[questionNumber].choices.length; i++) {
         var answerButtons = $("<button>").css("text-align", "left");
-        answerButtons.addClass("btn");
+        answerButtons.addClass("btn hello");
+        // answerButtons.attr("data-answer", questions[questionNumber].choices[i]);
+        answerButtons.attr("id",questions[questionNumber].choices[i]);
+        // answerButtons.attr("onclick","hello(this.id)");
         // $("#answers").append(i+1 + ")")
-        answerButtons.append(answerButtons.text(questions[questionNumber].choices[i]));
+        answerButtons.text(questions[questionNumber].choices[i]);
         $("#answerList").append("<li></li>");
         $("li:last").append(answerButtons);
         // $(answerListItem).append(answerButtons);
         // $("#answers").append("<br>");
+        // console.log(answerButtons.attr("id"));
     }
+    
+
+    $(".hello").click(function(event) {
+        // var mySelf = $(this);
+        alert(event.currentTarget.id);
+        if(event.currentTarget.id == questions[questionNumber].answer) {
+            alert("correct");
+            correctAnswersCount++;
+            alert(correctAnswersCount);
+        }
+        else {
+            alert("wrong");
+            timerSeconds -= 10;
+            wrongAnswersCount++;
+            alert(wrongAnswersCount);
+        }
+        nextQuestion();
+        // alert("you clicked a button " + answerButtons.attr("id"));
+        // alert(answerButtons.text($(this).attr("data-answer")));
+        // alert(answerButtons.attr("id"));
+    });
 }
+
+function calculateScore() {
+    calculatedScore = questions.length - wrongAnswersCount;
+}
+
+function scoreBoard() {
+    $(".card").empty();
+    calculateScore();
+    var scoreBoardDiv = $("<div class = 'card-body'>");
+    var scoreBoardTitle = $("<h2> High Scores</h2>").css("text-align","center");
+    scoreBoardTitle.addClass("card-header");
+    // scoreBoardDiv.append("<div class = 'container'>").append("<div class = 'row'>").append("<div class = 'col-6'>").append("<div class = 'col-6'>");
+    var score = $("<p>" + localStorage.getItem("initials") + " " + calculatedScore + "</p>");
+    score.css("margin-top", "3%");
+    scoreBoardDiv.append(scoreBoardTitle);
+    scoreBoardDiv.append(score);
+    $(".card").append(scoreBoardDiv);
+}
+
+function storeInitials() {
+    var inputInitials = $("<input type='text' placeholder = 'Type in initials'>");
+    $(".card").append("<div class = 'card-body'>");
+    $(".card-body").append(inputInitials); 
+    $(".card-body").append("</br><button id = 'submit' class = 'btn'>Submit</button>");
+    $("#submit").click(function(){
+        localStorage.setItem("initials", inputInitials.val());
+        scoreBoard();
+    })
+}
+
+
+
+// function hello(id) {
+//     alert(id);
+// }
+
+
+// function test(clickedID){
+//     alert(clickedID);
+//    }
+
 
